a784897d311d39d06952b89d087837a97876893e
androidwu <1462932319@qq.com>
2019-04-01 19:28:43 +0800

    init

---
+++ src/main/java/com/example/dubboservice/provider/DefaultDemoService.java
@@ -0,0 +1,21 @@
+package com.example.dubboservice.provider;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.example.api.model.User;
+import com.example.api.service.DemoService;
+
+import java.util.Date;
+
+@Service
+public class DefaultDemoService implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        return "hello,"+name+"(from Springboot2)";
+    }
+
+    @Override
+    public User sayHello2(User user) {
+        user.setBirthday(new Date());
+        return user;
+    }
+}
